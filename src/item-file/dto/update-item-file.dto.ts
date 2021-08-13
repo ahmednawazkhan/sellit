7b479583ba7f3b23d5dbb429cb2141ce00fab843
Ahmed Nawaz Khan <ahmed.nawaz@tenpearls.com>
2021-08-14 00:51:31 +0500

    updated update item file dto

--- src/item-file/dto/update-item-file.dto.ts
+++ src/item-file/dto/update-item-file.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateItemFileDto } from './create-item-file.dto';
 
-export type UpdateItemFileDto = Partial<CreateItemFileDto>
+// export type UpdateItemFileDto = Partial<CreateItemFileDto>
+
+export class UpdateItemFileDto extends PartialType(CreateItemFileDto) {}
